aca456ee94e3dc243243ae7aefa8db5b6bcf34f8
Rafael Giuriatto <rafagiuriatto@hotmail.com>
2024-07-07 13:57:59 -0300

    code to grep into files

--- src/lib.zig
+++ src/lib.zig
@@ -4,40 +4,24 @@ pub const InvokeError = error{
     ArgAlloc,
     NotEnoughArgs,
     TooManyArgs,
+    InvalidArg,
 };
 
 pub const Config = struct {
     query: []const u8,
-    path: []const u8,
 
     const Self = @This();
 
-    fn init(query: []const u8, path: []const u8) Self {
+    fn init(query: []const u8) Self {
         return .{
             .query = query,
-            .path = path,
         };
     }
 
-    pub fn search(self: Self) !void {
-        var file = try std.fs.cwd().openFile(self.path, .{});
-        defer file.close();
-
-        var buf_reader = std.io.bufferedReader(file.reader());
-        var reader = buf_reader.reader();
-
-        const stdout_file = std.io.getStdOut().writer();
-        var buf_writer = std.io.bufferedWriter(stdout_file);
-        const stdout = buf_writer.writer();
-
-        var buf: [1024]u8 = undefined;
-        while (try reader.readUntilDelimiterOrEof(&buf, '\n')) |line| {
-            if (std.mem.count(u8, line, self.query) > 0) {
-                try stdout.print("{s}\n", .{line});
-            }
-        }
-
-        try buf_writer.flush();
+    pub fn run(self: Self) !void {
+        var dir = try std.fs.cwd().openDir(".", .{ .iterate = true });
+        defer dir.close();
+        try iterateDir(dir, self.query);
     }
 };
 
@@ -51,8 +35,49 @@ pub fn readArgs() InvokeError!Config {
     _ = it.next();
 
     const query = it.next() orelse return InvokeError.NotEnoughArgs;
-    const path = it.next() orelse return InvokeError.NotEnoughArgs;
     if (it.next() != null) return InvokeError.TooManyArgs;
 
-    return Config.init(query, path);
+    if (query.len == 0) return InvokeError.InvalidArg;
+
+    return Config.init(query);
+}
+
+fn search(file: std.fs.File, query: []const u8, name: []const u8) !void {
+    var buf_reader = std.io.bufferedReader(file.reader());
+    var reader = buf_reader.reader();
+
+    const stdout_file = std.io.getStdOut().writer();
+    var buf_writer = std.io.bufferedWriter(stdout_file);
+    const stdout = buf_writer.writer();
+
+    var buf: [1024]u8 = undefined;
+    while (try reader.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        if (std.mem.count(u8, line, query) > 0) {
+            try stdout.print("{s}\n", .{name});
+            break;
+        }
+    }
+
+    try buf_writer.flush();
+}
+
+pub fn iterateDir(dir: std.fs.Dir, query: []const u8) !void {
+    var it = dir.iterate();
+
+    while (try it.next()) |entry| {
+        switch (entry.kind) {
+            std.fs.File.Kind.file => {
+                const file = try std.fs.cwd().openFile(entry.name, .{});
+                try search(file, query, entry.name);
+                file.close();
+            },
+            std.fs.File.Kind.directory => {
+                std.debug.print("{s}\n", .{entry.name});
+                // var sub_dir = try dir.openDir(entry.name, .{ .iterate = true });
+                // try iterateDir(sub_dir, query);
+                // sub_dir.close();
+            },
+            else => continue,
+        }
+    }
 }
--- src/main.zig
+++ src/main.zig
@@ -8,16 +8,20 @@ pub fn main() !void {
             return;
         },
         zg.InvokeError.NotEnoughArgs => {
-            std.debug.print("Not enough args. Usage: zgrep <query> <path>\n", .{});
+            std.debug.print("Not enough args. Usage: zgrep <query>\n", .{});
             return;
         },
         zg.InvokeError.TooManyArgs => {
-            std.debug.print("Too many args. Usage: zgrep <query> <path>\n", .{});
+            std.debug.print("Too many args. Usage: zgrep <query>\n <path>", .{});
+            return;
+        },
+        zg.InvokeError.InvalidArg => {
+            std.debug.print("Invalid arg. Use a valid string\n", .{});
             return;
         },
     };
 
-    config.search() catch |err| {
+    config.run() catch |err| {
         std.debug.print("Error reading file: {any}\n", .{err});
     };
 }
